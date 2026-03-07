386bdf91116a82295017ab583765e2beb977dcd2
Real Limoges <b.real.limoges@gmail.com>
2026-03-07 11:31:28 -0800

    add operators

--- src/Liminal/Core/Operators.hs
+++ src/Liminal/Core/Operators.hs
@@ -2,3 +2,12 @@ module Liminal.Core.Operators
 where
 
 
+fuzzyAnd :: TNorm t => t -> FuzzySet -> FuzzySet -> FuzzySet
+
+fuzzyOr :: TNorm t => t -> FuzzySet -> FuzzySet -> FuzzySet
+
+fuzzyNot :: FuzzySet -> FuzzySet
+
+very :: FuzzySet -> FuzzySet
+
+somewhat :: FuzzySet -> FuzzySet
